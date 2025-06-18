3ce370ca8b62a72341952b107e59b0ec165d682b
pharos23 <zephyrum377@protonmail.com>
2025-06-18 14:18:51 +0100

    App Title Update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ 'DaisyUI - SCM', config('app.name') }}</title>
 
     <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png">
 
