c386c6e0b4a0af34527543341fef15ddb84dd1dd
pharos23 <zephyrum377@protonmail.com>
2025-06-20 15:32:19 +0100

    Addition of language system (en & pt)

---
+++ app/Http/Middleware/DetectBrowserLocale.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Session;
+
+class DetectBrowserLocale
+{
+    protected $supportedLocales = ['en', 'pt'];
+
+    public function handle($request, Closure $next)
+    {
+        if (!Session::has('locale')) {
+            $browserLocale = substr($request->server('HTTP_ACCEPT_LANGUAGE'), 0, 2);
+
+            if (in_array($browserLocale, $this->supportedLocales)) {
+                Session::put('locale', $browserLocale);
+                App::setLocale($browserLocale);
+            } else {
+                Session::put('locale', config('app.locale'));
+                App::setLocale(config('app.locale'));
+            }
+        } else {
+            App::setLocale(Session::get('locale'));
+        }
+
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -6,71 +6,77 @@ use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\SettingController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\DetectBrowserLocale;
 use App\Models\Contact;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 // Routes to redirect the user around the page
 Auth::routes();
 
-Route::get('/', function () {
-    return redirect()->route('login');
+Route::middleware([
+    DetectBrowserLocale::class,
+])->group(function () {
+
+    Route::get('/', function () {
+        return redirect()->route('login');
+    });
+
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+    Route::middleware(['role:Admin|Super Admin'])->group(function () {
+        Route::resources([
+            'roles' => RoleController::class,
+            'users' => UserController::class,
+        ]);
+
+        Route::get('/user-search', [UserController::class, 'search'])
+            ->name('users.search');
+
+        Route::resource('permissions',
+            PermissionController::class)->only([
+            'index', 'store', 'destroy'
+        ]);
+
+        Route::get('/admin', function () {
+            return view('users.admin');
+        })->name('admin.dashboard');
+    });
+
+    Route::middleware(['auth'])->group(function () {
+        Route::get('/user', [SettingController::class, 'edit'])->name('userSettings.edit');
+        Route::put('/user', [SettingController::class, 'update'])->name('userSettings.update');
+    });
+
+    Route::group(['middleware' => ['permission:view-contact']], function () {
+
+        Route::get('/contacts', function () {
+            $contacts = Contact::query()->paginate(8);
+            return view('contacts.index', ['contacts' => $contacts]);
+        })->name('contacts');
+
+        Route::get('/search', [ContactController::class, 'search'])
+            ->name('contacts.search');
+
+        Route::put('/create', [ContactController::class, 'store'])
+            ->middleware('permission:create-contact')
+            ->name('contacts.create');
+
+        Route::get('/contacts/{id}', [ContactController::class, 'show'])
+            ->name('contacts.show');
+
+        Route::put('/contacts/{id}', [ContactController::class, 'update'])
+            ->middleware('permission:edit-contact')
+            ->name('contacts.update');
+
+        Route::put('/contacts/{id}/ticket', [ContactController::class, 'updateTicket'])
+            ->middleware('permission:edit-contact')
+            ->name('contacts.updateTicket');
+
+        Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])
+            ->middleware('permission:delete-contact')
+            ->name('contacts.destroy');
+    });
+
+    Route::get('/export-contacts', [ContactController::class, 'export'])->name('contacts.export');
+    Route::post('/contacts/import', [ContactController::class, 'import'])->name('contacts.import');
 });
-
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-
-Route::middleware(['role:Admin|Super Admin'])->group(function () {
-    Route::resources([
-        'roles' => RoleController::class,
-        'users' => UserController::class,
-    ]);
-
-    Route::get('/user-search', [UserController::class, 'search'])
-        ->name('users.search');
-
-    Route::resource('permissions',
-        PermissionController::class)->only([
-        'index', 'store', 'destroy'
-    ]);
-
-    Route::get('/admin', function () {
-        return view('users.admin');
-    })->name('admin.dashboard');
-});
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('/user', [SettingController::class, 'edit'])->name('userSettings.edit');
-    Route::put('/user', [SettingController::class, 'update'])->name('userSettings.update');
-});
-
-Route::group(['middleware' => ['permission:view-contact']], function () {
-
-    Route::get('/contacts', function () {
-        $contacts = Contact::query()->paginate(8);
-        return view('contacts.index', ['contacts' => $contacts]);
-    })->name('contacts');
-
-    Route::get('/search', [ContactController::class, 'search'])
-        ->name('contacts.search');
-
-    Route::put('/create', [ContactController::class, 'store'])
-        ->middleware('permission:create-contact')
-        ->name('contacts.create');
-
-    Route::get('/contacts/{id}', [ContactController::class, 'show'])
-        ->name('contacts.show');
-
-    Route::put('/contacts/{id}', [ContactController::class, 'update'])
-        ->middleware('permission:edit-contact')
-        ->name('contacts.update');
-
-    Route::put('/contacts/{id}/ticket', [ContactController::class, 'updateTicket'])
-        ->middleware('permission:edit-contact')
-        ->name('contacts.updateTicket');
-
-    Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])
-        ->middleware('permission:delete-contact')
-        ->name('contacts.destroy');
-});
-
-Route::get('/export-contacts', [ContactController::class, 'export'])->name('contacts.export');
-Route::post('/contacts/import', [ContactController::class, 'import'])->name('contacts.import');
