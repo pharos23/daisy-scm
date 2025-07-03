deb892d987fd7d307d9f2aa171fd7c52412fa2aa
JulianMarr <fabioribeiro1917@gmail.com>
2025-07-03 10:05:47 +0100

    Update to the icon in the Login and Register page

--- resources/js/pages/home.js
+++ resources/js/pages/home.js
@@ -1,5 +1,5 @@
-// JS File to call the scripts needed for this page (in this case: users page)
-import {setupSearchUser, updateWaitWheel} from "../event-handler/updateWaitWheel.js";
+// JS File to call the scripts needed for this page (in this case: home/dashboard page)
+import {updateWaitWheel} from "../event-handler/updateWaitWheel.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     updateWaitWheel();
