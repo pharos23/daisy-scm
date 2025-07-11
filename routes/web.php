378a5e490b00c4cb5888b3b7ba9844e96038c144
pharos23 <zephyrum377@protonmail.com>
2025-07-11 10:57:54 +0100

    Always redirect to login enforced.

--- routes/web.php
+++ routes/web.php
@@ -27,80 +27,81 @@ Route::get('/', function () {
     return redirect()->route('login');
 });
 
-// Home dashboard route
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-
-// ----------------------
-// Admin & Super Admin Only
-// ----------------------
-Route::middleware(['role:Admin|Super Admin'])->group(function () {
-
-    // Roles and Users resource controllers
-    Route::resources([
-        'roles' => RoleController::class,
-        'users' => UserController::class,
-    ]);
-
-    // User search & restore
-    Route::get('/user-search', [UserController::class, 'search'])->name('users.search');
-    Route::post('/users/{id}/restore', [UserController::class, 'restore'])->name('users.restore');
-
-    // Permissions: only index, store, destroy
-    Route::resource('permissions',
-        PermissionController::class)->only([
-        'index', 'store', 'destroy'
-    ]);
-});
-
 // ----------------------
 // Authenticated Users
 // ----------------------
 Route::middleware(['auth'])->group(function () {
-    // Force password change
-    Route::get('/password/change', [PasswordChangeController::class, 'showChangeForm'])->name('forcepassword.change');
-    Route::post('/password/change', [PasswordChangeController::class, 'update'])->name('forcepassword.update');
+
+    // Home dashboard route
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
 
     // User settings
     Route::get('/user', [SettingController::class, 'edit'])->name('userSettings.edit');
     Route::put('/user', [SettingController::class, 'update'])->name('userSettings.update');
-});
 
-// ----------------------
-// Contacts (Permission: view-contact)
-// ----------------------
-Route::group(['middleware' => ['permission:view-contact']], function () {
+    // Force password change
+    Route::get('/password/change', [PasswordChangeController::class, 'showChangeForm'])->name('forcepassword.change');
+    Route::post('/password/change', [PasswordChangeController::class, 'update'])->name('forcepassword.update');
 
-    Route::get('/contacts', [ContactController::class, 'index'])
-        ->name('contacts.index');
+    // ----------------------
+    // Admin & Super Admin Only
+    // ----------------------
+    Route::middleware(['role:Admin|Super Admin'])->group(function () {
 
-    Route::put('/create', [ContactController::class, 'store'])
-        ->middleware('permission:create-contact')
-        ->name('contacts.create');
+        // Roles and Users resource controllers
+        Route::resources([
+            'roles' => RoleController::class,
+            'users' => UserController::class,
+        ]);
 
-    Route::get('/contacts/{id}', [ContactController::class, 'show'])
-        ->name('contacts.show');
+        // User search & restore
+        Route::get('/user-search', [UserController::class, 'search'])->name('users.search');
+        Route::post('/users/{id}/restore', [UserController::class, 'restore'])->name('users.restore');
 
-    Route::put('/contacts/{id}', [ContactController::class, 'update'])
-        ->middleware('permission:edit-contact')
-        ->name('contacts.update');
+        // Permissions: only index, store, destroy
+        Route::resource('permissions',
+            PermissionController::class)->only([
+            'index', 'store', 'destroy'
+        ]);
+    });
 
-    Route::put('/contacts/{id}/ticket', [ContactController::class, 'updateTicket'])
-        ->middleware('permission:edit-contact')
-        ->name('contacts.updateTicket');
+    // ----------------------
+    // Contacts (Permission: view-contact)
+    // ----------------------
+    Route::group(['middleware' => ['permission:view-contact']], function () {
 
-    Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])
-        ->middleware('permission:deactivate-contact')
-        ->name('contacts.destroy');
+        Route::get('/contacts', [ContactController::class, 'index'])
+            ->name('contacts.index');
 
-    Route::post('/contacts/{id}/restore', [ContactController::class, 'restore'])
-        ->middleware('permission:restore-contact')
-        ->name('contacts.restore');
-});
+        Route::put('/create', [ContactController::class, 'store'])
+            ->middleware('permission:create-contact')
+            ->name('contacts.create');
 
-// ----------------------
-// Export / Import
-// ----------------------
-Route::group(['middleware' => ['permission:excel-contact']], function () {
-    Route::get('/export-contacts', [ContactController::class, 'export'])->name('contacts.export');
-    Route::post('/contacts/import', [ContactController::class, 'import'])->name('contacts.import');
+        Route::get('/contacts/{id}', [ContactController::class, 'show'])
+            ->name('contacts.show');
+
+        Route::put('/contacts/{id}', [ContactController::class, 'update'])
+            ->middleware('permission:edit-contact')
+            ->name('contacts.update');
+
+        Route::put('/contacts/{id}/ticket', [ContactController::class, 'updateTicket'])
+            ->middleware('permission:edit-contact')
+            ->name('contacts.updateTicket');
+
+        Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])
+            ->middleware('permission:deactivate-contact')
+            ->name('contacts.destroy');
+
+        Route::post('/contacts/{id}/restore', [ContactController::class, 'restore'])
+            ->middleware('permission:restore-contact')
+            ->name('contacts.restore');
+    });
+
+    // ----------------------
+    // Export / Import
+    // ----------------------
+    Route::group(['middleware' => ['permission:excel-contact']], function () {
+        Route::get('/export-contacts', [ContactController::class, 'export'])->name('contacts.export');
+        Route::post('/contacts/import', [ContactController::class, 'import'])->name('contacts.import');
+    });
 });
